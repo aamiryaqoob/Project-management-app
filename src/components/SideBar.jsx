a20f40aa659efb866e20d7a5085822fd39610e95
aamiryaqoob <aamiryaqoob965@gmail.com>
2024-08-05 19:27:24 +0500

    update

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+
+export default function SideBar() {
+    return (
+        < aside >
+            <h2>Your Projects</h2>
+            <div>
+                <button>+ Add Projects</button>
+            </div>
+            <ul></ul>
+        </aside >
+    )
+}
