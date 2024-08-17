45d4bf5c6b32316e7ab47f3b4a3ffcfb90d2c4c7
aamiryaqoob <aamiryaqoob965@gmail.com>
2024-08-18 02:06:59 +0500

    update

--- src/components/SelectedProjectDetails.jsx
+++ src/components/SelectedProjectDetails.jsx
@@ -1,3 +1,5 @@
+import Tasks from "./Tasks"
+
 export default function SelectedProjectDetails({ project, onDelete }) {
 
     const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
@@ -14,6 +16,6 @@ export default function SelectedProjectDetails({ project, onDelete }) {
             <p className="text-stone-400 ">{formattedDate}</p>
             <p className="text-stone-600 whitespace-pre-wrap">{project.des}</p>
         </header>
-        Task
+        <Tasks />
     </div>
 }
